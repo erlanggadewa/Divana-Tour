ad307fc4a4915559ef92fa5bad10360611f7df70
erlanggadewa <erlanggadewa7@gmail.com>
2023-02-14 04:57:43 +0700

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Database\Factories\UserFactory;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+
+    }
+}
